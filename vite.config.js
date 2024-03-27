bb71a75ab08fb6399c257af4c70e580a95696765
Abdennour Houssam <aouladabdenouur@gmail.com>
2024-03-27 11:36:17 +0000

    fix viteconfig

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'; // Correct import statement for the plugin
 
-// https://vitejs.dev/config/
-
-export default {
-  plugins: [reactPlugin()],
+// Define your Vite configuration
+export default defineConfig({
+  plugins: [react()], // Use react() instead of reactPlugin()
   build: {
     rollupOptions: {
       external: ['react-toastify'],
     },
   },
-};
+});
